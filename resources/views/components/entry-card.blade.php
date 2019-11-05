cd8817caac03cfffa0c83d7d5810b16d6aa39558
Daniel Mendoza <daniel.mendoza@americavoice.com>
2019-11-04 20:29:59 -0600

    Refactor entry-card to be independent of data context

--- resources/views/components/entry-card.blade.php
+++ resources/views/components/entry-card.blade.php
@@ -1,24 +1,20 @@
-@foreach($userEntries as $entry)
-    <article class="card mb-2">
-        <header class="card-header">
-            <h5>{{ $entry['entry']['title'] }}</h5>
-            <h6 class="card-subtitle text-muted">
-                {{ $entry['entry']['created_at'] }}
-            </h6>
-        </header>
-        <div class="card-body">
-            <p class="card-text">{{ $entry['entry']['content'] }}</p>
-            <a href="{{ route('showEntry', [$entry['entry']['id']]) }}" class="card-link">{{ __('View') }}</a>
-            @auth
-                @if(auth()->user()->id === $entry['user']['id'])
-                    <a href="{{ route('showEditForm', [$entry['entry']['id']]) }}"
-                       class="card-link">{{ __('Edit') }}</a>
-                @endif
-            @endauth
-        </div>
-        <div class="card-footer text-muted">
-            <a class="text-decoration-none text-dark" href="{{ route('showUserEntries', [$entry['user']['id']]) }}">{{ __('By:') }}
-                <strong>{{ $entry['user']['name'] }}</strong></a>
-        </div>
-    </article>
-@endforeach
+<article class="card mb-2">
+    <header class="card-header">
+        <h5>{{ $title }}</h5>
+        <h6 class="card-subtitle text-muted">
+            <a class="text-decoration-none text-dark"
+               href="{{ $userEntriesLink }}">{{ __('By:') }}
+                <strong>
+                    {{ $user }}
+                </strong>
+            </a> {{ __('on') }} {{ $date }}
+        </h6>
+    </header>
+    <div class="card-body">
+        <p class="card-text">{{ $content }}</p>
+        <a href="{{ $showEntryLink }}" class="card-link">{{ __('View') }}</a>
+        @auth
+            {{ $editLink }}
+        @endauth
+    </div>
+</article>
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,8 +7,22 @@
                     <h1>{{ __('What\'s Going On?') }}</h1>
                 </header>
                 {{ $entries->links() }}
-                @component('components.entry-card', ['userEntries' => $entries])
-                @endcomponent
+                @foreach($entries as $entry)
+                    @component('components.entry-card')
+                        @slot('title'){{ $entry['entry']['title'] }}@endslot
+                        @slot('userEntriesLink'){{ route('showUserEntries', [$entry['user']['id']]) }}@endslot
+                        @slot('user'){{ $entry['user']['name'] }}@endslot
+                        @slot('date'){{ $entry['entry']['created_at'] }}@endslot
+                        @slot('content'){{ $entry['entry']['content'] }}@endslot
+                        @slot('showEntryLink'){{ route('showEntry', [$entry['entry']['id']]) }}@endslot
+                        @slot('editLink')
+                            @if(auth()->user()->id === $entry['user']['id'])
+                                <a href="{{ route('showEditForm', [$entry['entry']['id']]) }}"
+                                   class="card-link">{{ __('Edit') }}</a>
+                            @endif
+                        @endslot
+                    @endcomponent
+                @endforeach
                 {{ $entries->links() }}
             </article>
         </div>
