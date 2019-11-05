f82983e3c64db925216f215f27600601dbaebefb
Daniel Mendoza <daniel.mendoza@americavoice.com>
2019-11-04 21:34:52 -0600

    Add Dashboard test case

---
+++ tests/Feature/DashboardControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DashboardControllerTest extends TestCase
+{
+    public function testDashboard()
+    {
+        $user = new User();
+        $user->id = 1;
+        $user->name = 'TestUSer';
+        $response = $this->be($user)->get(route('dashboard'));
+        $response->assertStatus(200);
+    }
+}
