36a5518e5eeb02849227b9521cda4e667040f4fc
Daniel Mendoza <daniel.mendoza@americavoice.com>
2019-11-07 20:49:43 -0600

    Add function to mark hidden tweets

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Entry;
+use App\HiddenTweets;
+use App\HiddenTweets as HiddenTweetsAlias;
 use App\User;
 use DG\Twitter\Exception;
 use DG\Twitter\Twitter;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class UserController extends Controller
@@ -13,8 +16,12 @@ class UserController extends Controller
     /** @var Twitter */
     private $twitterClient;
 
-    public function __construct(Twitter $twitterClient)
+    /** @var HiddenTweetsAlias */
+    private $hiddenTweets;
+
+    public function __construct(Twitter $twitterClient, HiddenTweets $hiddenTweets)
     {
+        $this->hiddenTweets = $hiddenTweets;
         $this->twitterClient = $twitterClient;
     }
 
@@ -22,6 +29,7 @@ class UserController extends Controller
     {
         $entries = Entry::with('user')->where('user_id', '=', $user->id)->paginate(3);
         $userTweets = $this->getUserTweets($user->twitter_username);
+        dd($userTweets);
         return view('user.entries', ['entries' => $entries, 'user' => $user, 'tweets' => $userTweets]);
     }
 
@@ -31,15 +39,35 @@ class UserController extends Controller
             return [];
         }
         try {
-            return $this->twitterClient->request('/statuses/user_timeline.json', 'GET', [
+            $userTweets = $this->twitterClient->request('/statuses/user_timeline.json', 'GET', [
                 'count' => 5,
                 'screen_name' => $twitterUserName,
                 'exclude_replies' => true,
             ]);
+
+            return $this->markHiddenTweets($userTweets);
         } catch (Exception $e) {
             report($e);
 
             return [];
         }
+    }
+
+    private function markHiddenTweets(array $tweets): array
+    {
+        $hiddenTweets = $this->hiddenTweets::with('user')
+            ->where('user_id', '=', Auth::user()->id)
+            ->get(['tweet_id'])
+            ->map(function (HiddenTweets $item) {
+                return $item->tweet_id;
+            })->toArray();
+
+        foreach ($tweets as $tweet) {
+            if (in_array($tweet->id, $hiddenTweets)) {
+                $tweet->hidden = true;
+            }
+        }
+
+        return $tweets;
     }
 }
